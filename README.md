d3e87f1924c66923040076f1feceedecf3c7c7cf
JEN-dotcom <johnenyinwa@gmail.com>
2021-11-13 21:28:20 +0100

    chat-app

---
+++ README.md
@@ -0,0 +1,21 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## ReactJS Chat Application [Chatengine.io]
+
+A fully customizable chat application using [chatengine.io](https://chatengine.io/)
+
+[Test the app]() using these login credetials:
+
+### username: `Mr John` password: 123456 (Admin)
+### username: `Wendy` password: 123456
+
+Login as another user to test various functionalities
+
+
+### User Story:
+
+- User can view who read the messages
+- User can create new chats
+- Admin can add new people to chat
+
+### Snapshot
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+
+import './App.css';
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="ca60cbe4-aacc-4990-ab50-305aa1028e9d"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    );
+}
+
+export default App;
